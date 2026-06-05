2bc30fb754fbdb8df42a25f0951aea59191f80ec
Adam <The1nk@users.noreply.github.com>
2026-06-05 11:52:23 -0400

    feat(editor): NodeTargetType category->target-type mapping

---
+++ BotBuilder.Core.Tests/Targets/NodeTargetTypeTests.cs
@@ -0,0 +1,24 @@
+using AdbCore.Models;
+using BotBuilder.Core.Targets;
+using Xunit;
+
+namespace BotBuilder.Core.Tests.Targets;
+
+public class NodeTargetTypeTests
+{
+    [Theory]
+    [InlineData("Android", BotTargetType.AndroidDevice)]
+    [InlineData("Browser", BotTargetType.Browser)]
+    [InlineData("Screen", BotTargetType.Window)]
+    [InlineData("Input", BotTargetType.Window)]
+    public void For_KnownCategories_MapToTargetType(string category, BotTargetType expected)
+        => Assert.Equal(expected, NodeTargetType.For(category));
+
+    [Theory]
+    [InlineData("Control Flow")]
+    [InlineData("Data")]
+    [InlineData("Scripting")]
+    [InlineData("Whatever")]
+    public void For_TargetAgnosticCategories_ReturnNull(string category)
+        => Assert.Null(NodeTargetType.For(category));
+}
